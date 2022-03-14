52148e98fd53197bc7c09e608d59ab0155b93852
Пользователь <gorodov.v.v@mail.com>
2022-03-14 23:01:12 +0300

    Удалена директория node

---
+++ DB.js
@@ -0,0 +1,12 @@
+const db = {
+    cats: [
+        { name: "kitten",
+        link: "https://rozetked.me/images/uploads/dwoilp3BVjlE.jpg"
+         },
+    { name: "big cat",
+    link: "https://n1s1.elle.ru/48/7b/36/487b36300c62c5f0cb905da52aa874b4/728x486_1_30b570c2f6c0da65bb56095068e05768@940x627_0xc0a839a4_18087198581488362059.jpeg"
+    },
+          ],
+}
+
+module.exports = {db,}
--- app.js
+++ app.js
@@ -2,14 +2,40 @@ const express = require('express')
 
 const app = express()
 
+const { db } = require('./DB')
+
 const path = require('path')
+
+app.use(express.urlencoded( { extended: true}))
 
 app.set('views',path.join(__dirname, 'src', 'views'))
 
 app.set('view engine', 'hbs')
 
+app.get('/',  (req, res) => {
+  const catsQuery = req.query.limit
+  res.render('index.hbs', {kotiki: db.cats.slice(0, catsQuery)})
+  
+ })
+
+ app.get('/',  (req, res) => {
+  const catsReverse = req.query.reverse
+  if (catsReverse == true)
+    {res.render('index.hbs', {kotiki: db.cats.reverse()})}
+ })
+
 app.get('/', function (req, res) {
-  res.render('index.hbs')
+  res.render('index.hbs', {kotiki: db.cats})
 })
+
+
+app.post('/catslist', (req, res) => {
+  const dataFromForm = req.body
+
+  db.cats.push(dataFromForm)
+
+  res.redirect('/')
+})
+
 
 app.listen(3000)
