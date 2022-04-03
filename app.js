fb1efd6cc6e09d394ac6c9858ede7aace0325138
Пользователь <gorodov.v.v@mail.com>
2022-04-03 21:23:51 +0300

    Почти доделал кнопку удаления поста

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const { db } = require('./DB');
 const { db2 } = require('./DBusers');
 const sessions = require('express-session')
 const bcrypt = require('bcrypt')
-const {checkAuth} = require('./src/midlewares/checkAuth')
+const { checkAuth } = require('./src/midlewares/checkAuth')
 app.use(express.urlencoded({ extended: true }));
 const secretKey = 'qazswdblptsvhfedgsjusavj'
 const saltRounds = 10
 const hbs = require('hbs');
 const { count } = require('console');
+const res = require('express/lib/response');
 
 app.use(express.static(path.join(__dirname, "public")))
 
@@ -35,12 +36,12 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, 'src', 'views', 'partials'));
 
 app.use((req, res, next) => {
- 
-const currentEmail = req.session?.user?.email
+
+  const currentEmail = req.session?.user?.email
 
   if (currentEmail) {
     const currentUser = db2.users.find((user) => user.email === currentEmail)
-    
+
     res.locals.name = currentUser.name
   }
 
@@ -61,7 +62,7 @@ app.get('/secret', (req, res) => {
 // если надо включить лимит на посты - раскоментить
 */
 app.get('/', (req, res) => {
-  res.render('index.hbs', );
+  res.render('index.hbs',);
 });
 
 app.get('/auth/signup', (req, res) => {
@@ -79,7 +80,7 @@ app.post('/auth/signup', async (req, res) => {
     password: hashPass,
   });
 
-   req.session.user = {
+  req.session.user = {
     email
   }
 
@@ -97,14 +98,14 @@ app.post('/auth/signin', async (req, res) => {
 
   if (urrentUser) {
     if (await bcrypt.compare(password, currentUser.password)) {
-     
-            req.sesion.user = {
+
+      req.sesion.user = {
         email
       }
       return res.redirect('/');
     }
   }
- return res.redirect('/')
+  return res.redirect('/')
 });
 
 app.get("/insta", checkAuth, (req, res) => {
@@ -115,26 +116,26 @@ app.post('/insta', (req, res) => {
   const dataFromForm = req.body;
   const currentUser = req.session.id
   dataFromForm.sidFromUser1 = currentUser
-    db.cats.push(dataFromForm);
+  db.cats.push(dataFromForm);
   // добавляем новый пост, имя юзера записываем в массив вместе с котиком
   res.redirect('/secret');
 });
 
-app.get('/auth/signout', (req,res) => {
-/*const sisFromUserCookie = req.cookies.sid
+app.get('/auth/signout', (req, res) => {
+  /*const sisFromUserCookie = req.cookies.sid
+  
+  delete sesions[sisFromUserCookie]
+  
+  res.clearCookie('sid')
+  res.redirect('/')
+  */
 
-delete sesions[sisFromUserCookie]
+  req.session.destroy((err) => {
+    if (err) return res.redirect('/')
 
-res.clearCookie('sid')
-res.redirect('/')
-*/
-
-req.session.destroy((err) => {
-  if (err) return res.redirect('/')
-
-  res.clearCookie(req.app.get('cookieName'))
-  return res.redirect('/')
-})
+    res.clearCookie(req.app.get('cookieName'))
+    return res.redirect('/')
+  })
 
 })
 
@@ -153,6 +154,23 @@ app.post("/secret", checkAuth, (req, res) => {
   res.redirect('/secret');
 }) // попытка сделать кнопку удаления поста через кнопку отправки формы
 */
+
+app.patch('/secret/:id', (req, res) => {
+  const id = req.params.id
+  //const { action } = req.body
+
+  // const curentPost = db.cats.sidFromUser1.find((el) => db.cats.sidFromUser1 === id)
+  const curentPostId = db.cats.findIndex((el) => el.sidFromUser1 === id)
+  if (req.session.id === id) {
+
+    db.cats.splice(curentPostId, 1)
+  }
+
+  else
+
+    res.sendStatus(403)
+
+})
 app.listen(3000, () => {
   console.log('server start success');
 });
--- public/main.js
+++ public/main.js
@@ -5,14 +5,20 @@ const deleteButton = document.querySelector('[data-post]')
 
 deleteButton.addEventListener('click', async (e) => {
 
-if (e.target.dataset.action) {
-    console.log(e.target)
-    const targ = e.target.dataset.id
+    if (e.target.dataset.action) {
+        console.log(e.target)
+        const targ = e.target.dataset.id
 
-    const response = await fetch(`/secret/${targ}`, {
-        
-    })
+        const response = await fetch(`/secret/${targ}`, {
+            method: "PATCH",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                action: e.target.dataset.action
+            })
+        })
 
-console.log(targ)
-}
+        console.log(targ)
+    }
 })
